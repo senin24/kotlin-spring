cfa0000ca3dfbc113a447a13983188ce3345f83d
senin24 <senin24@gmail.com>
2020-04-30 17:26:39 +0300

    init commit

---
+++ src/main/kotlin/com/git/senin24/kotlinpoc/csvtobd/CsvtobdApplication.kt
@@ -0,0 +1,11 @@
+package com.git.senin24.kotlinpoc.csvtobd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CsvtobdApplication
+
+fun main(args: Array<String>) {
+	runApplication<CsvtobdApplication>(*args)
+}
---
+++ src/test/kotlin/com/git/senin24/kotlinpoc/csvtobd/CsvtobdApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.git.senin24.kotlinpoc.csvtobd
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CsvtobdApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
